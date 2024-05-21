98ee09661e177ef1c8b686ce53f742e94e4974ae
Andreas Fredriksson <afredriksson89@gmail.com>
2024-05-21 10:33:37 +0200

    UpdateUser added

---
+++ AccountProvider/Functions/UpdateOneUser.cs
@@ -0,0 +1,88 @@
+using AccountProvider.Models;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AccountProvider.Functions
+{
+    public class UpdateOneUser
+    {
+        private readonly ILogger<UpdateOneUser> _logger;
+        private readonly UserManager<UserAccount> _userManager;
+
+        public UpdateOneUser(ILogger<UpdateOneUser> logger, UserManager<UserAccount> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [Function("UpdateOneUser")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "put")] HttpRequest req)
+        {
+
+            try
+            {
+                string body = await new StreamReader(req.Body).ReadToEndAsync();
+                UpdateOneUserModel? userModel = JsonConvert.DeserializeObject<UpdateOneUserModel>(body);
+
+                if (string.IsNullOrEmpty(userModel?.Email))
+                {
+                    return new BadRequestObjectResult("No email provided");
+                }
+
+                UserAccount? user = _userManager.FindByEmailAsync(userModel.Email).Result;
+
+                if (user == null)
+                {
+                    return new BadRequestObjectResult("User not found");
+                }
+
+                if (!string.IsNullOrEmpty(userModel.NewEmail))
+                {
+                    user.Email = userModel.NewEmail;
+                }
+
+                if (!string.IsNullOrEmpty(userModel.NewPassword))
+                {
+                    user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userModel.NewPassword);
+                }
+
+                if (!string.IsNullOrEmpty(userModel.FirstName))
+                {
+                    user.FirstName = userModel.FirstName;
+                }
+
+                if (!string.IsNullOrEmpty(userModel.LastName))
+                {
+                    user.LastName = userModel.LastName;
+                }
+
+                if (!string.IsNullOrEmpty(userModel.PhoneNumber))
+                {
+                    user.PhoneNumber = userModel.PhoneNumber;
+                }
+
+                IdentityResult result = _userManager.UpdateAsync(user).Result;
+
+                if (result.Succeeded)
+                {
+                    return new OkObjectResult("User updated");
+                }
+                else
+                {
+                    return new BadRequestObjectResult("User could not be updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"UpdateOneUser :: {ex.Message}");
+                return new BadRequestObjectResult(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ AccountProvider/Models/UpdateOneUserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountProvider.Models;
+
+public class UpdateOneUserModel
+{
+    public string? Email { get; set; } 
+    public string? NewEmail { get; set; } 
+    public string? Password { get; set; } 
+    public string? NewPassword { get; set; }
+    public string? FirstName { get; set; } 
+    public string? LastName { get; set; } 
+    public string? PhoneNumber { get; set; }
+}
